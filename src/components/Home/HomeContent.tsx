2cc79e24b144c60880b38fd039e781c961fbb180
sdldev <sidolapakdev@gmail.com>
2024-01-08 22:56:58 +0700

    Update v1.0.1 Beta

--- src/components/Home/HomeContent.tsx
+++ src/components/Home/HomeContent.tsx
@@ -37,12 +37,12 @@ const HomeContent = () => {
         initial="hidden"
         animate="visible"
         variants={containerHomeVariants}
-        className="flex flex-col items-center text-center"
+        className="text-white flex flex-col items-center text-center"
       >
           <h1 className="mx-2 xl:text-[82px] md:text-[70px] text-[32px] font-bold">
-            Santri <span className="text-warning">MU</span> 
+            Santri <span className="text-green-600">MU</span> 
           </h1>
-          <h2 className="text-warning xl:text-5xl md:text-4xl text-xl">Aplikasi Terpadu Santri Boarding School</h2>
+          <h2 className="text-gray-300 xl:text-5xl md:text-4xl text-xl">Aplikasi Terpadu Santri Boarding School</h2>
         <div className="relative -z-10">
         </div>
       </motion.div>
@@ -52,7 +52,14 @@ const HomeContent = () => {
         variants={descriptionHomeVariants}
         className="md:text-[24px] max-w-3xl  md:leading-10 leading-6"
       >
-        <h3 className="text-sm text-center">Company Profil, LandingPage, CMS, dan Aplikasi Berbasis Web</h3>
+        <h3 className="text-gray-300 text-sm text-center">
+          Aplikasi Satri-MU digagas untuk memenuhi 
+          kebutuhan management dan sistem informasi pondok, baik untuk pengelola, yayasan, maupun wali santri untuk wali santri 
+          </h3>
+
+          <p className="text-green-500 text-bold text-center pt-10">
+          v.1.0.1 Beta
+          </p>
       </motion.div>
     </>
   );
