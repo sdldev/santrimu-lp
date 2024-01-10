eaa3a963efacad1a6027e0166d51460141707214
sdldev <sidolapakdev@gmail.com>
2024-01-10 17:37:49 +0700

    Update Motion

--- src/components/Home/HomeTable.tsx
+++ src/components/Home/HomeTable.tsx
@@ -7,6 +7,7 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import { motion, AnimatePresence } from 'framer-motion'
 
 const problems = [
     {
@@ -36,57 +37,65 @@ const problems = [
 ]
 export function HomeTable() {
     return (
-
-
-        <div
-            className=" py-8 leading-normal  sm:text-lg sm:leading-7"
-        >
-            <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
-                Latar Belakang
-            </h2>
-            <p
-                className="max-w-full leading-normal text-muted-foreground sm:text-lg sm:leading-7"
-            >
-                Aplikasi ini di gagas berdasarkan keadaan di lapangan, yang menjadi
-                kebutuhan pengelola pondok, wali pendamping <i>(ustaz/ustazah)</i>, dan
-                wali santri.
-            </p>
-
-            <p
-                className="max-w-full leading-normal text-muted-foreground sm:text-lg sm:leading-7"
-            >
-                Kami sebagai tim pengembang, sangat mengharapkan masukan dan bantuan
-                kepada semua pihak, demi berkembangnya aplikasi ini, sehingga applikasi
-                ini bisa bermanfaat untuk kita semua
-            </p>
-
-
-            <div
-                className="py-8 leading-normal text-muted-foreground sm:text-lg sm:leading-7"
-            >
-                <Table>
-                    <TableCaption className="text-sm text-red-800">*desclaimer: data tidak mewakili data manapun dan dari pihak manapun. data hanya berupa contoh</TableCaption>
-                    <TableHeader>
-                        <TableRow className="text-xl">
-                            <TableHead >Problem</TableHead>
-                            <TableHead>Solusi</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {problems.map((problem) => (
-                            <TableRow key={problem.invoice}>
-                                <TableCell>{problem.invoice}</TableCell>
-                                <TableCell>{problem.paymentStatus}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </div>
-
-        </div>
-
-
-
+        <AnimatePresence>
+            <section id="about" className="container py-8">
+                <div
+                    className=" py-8 leading-normal  sm:text-lg sm:leading-7"
+                >
+                    <motion.h2
+                        initial={{ opacity: 0, y: 30 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.3, delay: 0.1 }}
+                        className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+                        Latar Belakang
+                    </motion.h2>
+                    <motion.p
+                        initial={{ opacity: 0, y: 30 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.3, delay: 0.3 }}
+                        className="max-w-full leading-normal text-muted-foreground sm:text-lg sm:leading-7"
+                    >
+                        Aplikasi ini di gagas berdasarkan keadaan di lapangan, yang menjadi
+                        kebutuhan pengelola pondok, wali pendamping <i>(ustaz/ustazah)</i>, dan
+                        wali santri.
+                    </motion.p>
+                    <motion.p
+                        initial={{ opacity: 0, y: 30 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.3, delay: 0.5 }}
+                        className="max-w-full leading-normal text-muted-foreground sm:text-lg sm:leading-7"
+                    >
+                        Kami sebagai tim pengembang, sangat mengharapkan masukan dan bantuan
+                        kepada semua pihak, demi berkembangnya aplikasi ini, sehingga applikasi
+                        ini bisa bermanfaat untuk kita semua
+                    </motion.p>
+                    <motion.div
+                        initial={{ opacity: 0, y: -100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5, delay: 0.7 }}
+                        className="py-8 leading-normal text-muted-foreground sm:text-lg sm:leading-7"
+                    >
+                        <Table>
+                            <TableCaption className="text-sm text-red-800">*desclaimer: data tidak mewakili data manapun dan dari pihak manapun. data hanya berupa contoh</TableCaption>
+                            <TableHeader>
+                                <TableRow className="text-xl">
+                                    <TableHead >Problem</TableHead>
+                                    <TableHead>Solusi</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {problems.map((problem) => (
+                                    <TableRow key={problem.invoice}>
+                                        <TableCell>{problem.invoice}</TableCell>
+                                        <TableCell>{problem.paymentStatus}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </motion.div>
+                </div>
+            </section>
+        </AnimatePresence>
     )
 }
 export default HomeTable;
