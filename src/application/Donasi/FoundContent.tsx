4c93069335d56970792905fc4ecde17fbe288776
sdldev <sidolapakdev@gmail.com>
2024-08-09 16:13:26 +0700

    add saweria

--- src/application/Donasi/FoundContent.tsx
+++ src/application/Donasi/FoundContent.tsx
@@ -1,56 +1,61 @@
-import { FadeIn } from "@/components/FadeIn";
-import { Container } from "@/components/Container";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-const banks = [
-  ['1132142132', 'Bank Lampung', '/teams/bank-lampung.webp', 'a/n Santrimu'],
-  [ '43245434663', 'Bank Mandiri', '/teams/mandiri.webp', 'a/n Santrimu'],
-  ['346355654', 'Bank BRI', '/teams/bri.webp', 'a/n Santrimu'],
-  ['679645633', 'Bank BSI', '/teams/bsi.webp', 'a/n Santrimu'],
-  ['8643563434', 'Bank BNI', '/teams/bni.webp', 'a/n Santrimu'],
-]
+import { motion, AnimatePresence } from 'framer-motion'
+
+
 const AboutContent = () => {
   return (
-    <Container>
-      <FadeIn className="mx-auto flex max-w-[58rem] flex-col items-center justify-center gap-4">
-        <h2 className="font-heading text-2xl py-12 font-semibold leading-[1.1] sm:text-3xl md:text-5xl">
-        Salurkan donasi Anda
-        </h2>
-        <p className="mx-auto text-center w-4/6 leading-normal text-muted-foreground sm:text-lg sm:leading-7">
-        مَثَلُ الَّذِيْنَ يُنْفِقُوْنَ اَمْوَالَهُمْ فِيْ سَبِيْلِ اللّٰهِ كَمَثَلِ حَبَّةٍ اَنْۢبَتَتْ سَبْعَ سَنَابِلَ فِيْ كُلِّ سُنْۢبُلَةٍ مِّائَةُ حَبَّةٍ ۗ وَاللّٰهُ يُضٰعِفُ لِمَنْ يَّشَاۤءُ ۗوَاللّٰهُ وَاسِعٌ عَلِيْمٌ 
+    <AnimatePresence>
+      <section id="home" className="container py-8">
+        <div
+          className="container flex max-w-[64rem] h-screen flex-col items-center gap-5 text-center"
+        >
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.2 }}
+            className="flex xl:justify-center">
+            <img
+              src="/images/saweria-santrimu.png"
+              decoding="async"
+              loading="lazy"
+              alt="Santri-MU"
+              width="300"
+              height="300"
+              className="mt-1 max-w-full"
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.5 }}
+            className="flex flex-col items-center text-center"
+          >
+            <h1
+              className="font-heading text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl !leading-[1.1] text-balance"
+            >
+              <span className="text-gradient_indigo-purple">
+                Dukung Project Santrimu di Saweria
+              </span>
+            </h1>
+            <h2 className="pt-12 max-w-[42rem] leading-normal sm:text-xl sm:leading-8 text-2xl">
+            <a href="https://saweria.co/santrimu" target="_blank">https://saweria.co/santrimu</a> 
+            </h2>
+            <div className="relative -z-10"></div>
+          </motion.div>
 
-        </p>
-        <p className="mx-auto text-center w-5/6 leading-normal text-muted-foreground text-xs md:text-sm sm:leading-7">
-        Artinya: "Perumpamaan orang-orang yang menginfakkan hartanya di jalan Allah adalah seperti (orang-orang yang menabur) sebutir biji (benih) yang menumbuhkan tujuh tangkai, pada setiap tangkai ada seratus biji. Allah melipatgandakan (pahala) bagi siapa yang Dia kehendaki. Allah Maha Luas lagi Maha Mengetahui."
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.7 }}
+            className="flex flex-col items-center text-center leading-normal text-muted-foreground sm:text-xl sm:leading-8 text-balance"
+          >
+            <p className="text-green-500 text-bold text-center pt-10">
+              v.1.0.1
+            </p>
+          </motion.div>
 
-        </p>
-    <div className="rounded-4xl py-16 md:py-24">
-
-          <ul role="list" className="mt-6 grid gap-x-32 gap-y-10 lg:grid-cols-3">
-
-            {banks.map(([norek, bank, logo, rekening]) => (
-              <li key={norek}>
-                <FadeIn>
-                  <div className="flex items-center">
-                    <Avatar className="h-16 w-16">
-                      <AvatarImage src={logo} alt={norek} />
-                      <AvatarFallback>MBS</AvatarFallback>
-                    </Avatar>
-                    <div className="ml-4 space-y-1">
-                      <p className="text-sm leading-none">{bank}</p>
-                      <p className="text-xs text-muted-foreground">{norek}</p>
-                      <p className="text-xs">
-                        {rekening}
-                      </p>
-                    </div>
-                  </div>
-
-                </FadeIn>
-              </li>
-            ))}
-          </ul>
-    </div>
-      </FadeIn>
-    </Container>
+        </div>
+      </section>
+    </AnimatePresence>
   );
 };
 export default AboutContent;
